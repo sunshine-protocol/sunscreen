f9b79b442f2da29f10cb0e6a551c608daeff46e1
Shady Khalifa <shekohex@gmail.com>
2020-07-25 16:30:16 +0200

    ffi gen code

--- packages/sunshine/lib/ffi.dart
+++ packages/sunshine/lib/ffi.dart
@@ -6,7 +6,6 @@ import 'package:ffi/ffi.dart' as ffi;
 
 // ignore_for_file: unused_import, camel_case_types, non_constant_identifier_names
 final DynamicLibrary _dl = _open();
-
 /// Reference to the Dynamic Library, it should be only used for low-level access
 final DynamicLibrary dl = _dl;
 DynamicLibrary _open() {
@@ -19,9 +18,7 @@ DynamicLibrary _open() {
 void link_me_please() {
   _link_me_please();
 }
-
-final _link_me_please_Dart _link_me_please = _dl
-    .lookupFunction<_link_me_please_C, _link_me_please_Dart>('link_me_please');
+final _link_me_please_Dart _link_me_please = _dl.lookupFunction<_link_me_please_C, _link_me_please_Dart>('link_me_please');
 typedef _link_me_please_C = Void Function();
 typedef _link_me_please_Dart = void Function();
 
@@ -31,10 +28,7 @@ void store_dart_post_cobject(
 ) {
   _store_dart_post_cobject(ptr);
 }
-
-final _store_dart_post_cobject_Dart _store_dart_post_cobject = _dl
-    .lookupFunction<_store_dart_post_cobject_C, _store_dart_post_cobject_Dart>(
-        'store_dart_post_cobject');
+final _store_dart_post_cobject_Dart _store_dart_post_cobject = _dl.lookupFunction<_store_dart_post_cobject_C, _store_dart_post_cobject_Dart>('store_dart_post_cobject');
 typedef _store_dart_post_cobject_C = Void Function(
   Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>> ptr,
 );
