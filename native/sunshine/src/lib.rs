0a46fcb8cc2bf24fdfa955530c2cbc05082982e4
Shady Khalifa <shekohex@gmail.com>
2020-08-04 21:50:05 +0200

    dose not compile waiting for https://github.com/sunshine-protocol/sunshine/issues/18

--- native/sunshine/src/lib.rs
+++ native/sunshine/src/lib.rs
@@ -1,3 +1,19 @@
+use client::Client;
+use frusty_logger::{Config, FilterBuilder};
+use log::Level;
+
+frusty_logger::include_ffi!(
+  with_config: Config::new(
+    Level::Info,
+    FilterBuilder::new()
+    .parse("sunshine,sc_information,substrate_subxt")
+    .build()
+  )
+);
+
+identity_ffi::impl_ffi!(client: Client);
+bounty_ffi::impl_ffi!();
+
 /// a hack to make iOS link to this lib
 #[inline(never)]
 #[no_mangle]
